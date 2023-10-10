ad23785523ed45cee3ef40fafe8c46e71220727e
Md Redwan <136357607+RedwanHossain96@users.noreply.github.com>
2023-10-10 21:39:49 +0600

    Add files via upload

---
+++ DAL/OrderContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Exam_7.Models;
+
+namespace Exam_7.DAL
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext() : base("Exam_DB")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderMaster> OrderMasters { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+    }
+}
